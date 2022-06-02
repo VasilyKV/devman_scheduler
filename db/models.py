0be812950055e512a9747f639e5a887f4ade0097
Victor Abdukarimov <viredmi@gmail.com>
2022-06-02 23:57:07 +0300

    Added scripts for add PM for BD

--- db/models.py
+++ db/models.py
@@ -1,30 +1,35 @@
 from django.db import models
-from django.db.models.fields import DateTimeField, IntegerField
-
-class Projects(models.Model):
-    name = models.CharField(max_length=100)
-    date_start = DateTimeField()
-    date_end = DateTimeField()
 
 
-class Product_managers(models.Model):
-    name = models.CharField(max_length=50)
-    tg_id = IntegerField()
-    tg_nick = models.CharField(max_length=50)
-    time_slot = models.DateTimeField()
+
+class ProductManagers(models.Model):
+    pm_name = models.CharField(max_length=50)
+    pm_tg_id = models.IntegerField()
+    pm_tg_username = models.CharField(max_length=50)
+    start_work_time = models.DateTimeField()
+    end_work_time = models.DateTimeField()
 
 
 class Students(models.Model):
-    name = models.CharField(max_length=50)
-    tg_id = IntegerField()
-    level = models.IntegerField()
-    time_slot = models.DateTimeField()
+    std_name = models.CharField(max_length=50)
+    std_tg_id = models.IntegerField()
+    std_tg_username = models.CharField(max_length=50)
+    level = models.CharField(max_length=50)
+    wanted_time = models.JSONField()
+
+
+class Projects(models.Model):
+    project_name = models.TextField()
+    project_start_date = models.DateTimeField()
+    project_end_date = models.DateTimeField()
 
 
 class Teams(models.Model):
-    team_id = models.TextField
-    pm_tg_name = models.CharField(max_length=100)
-    pm_name = models.ForeignKey(Product_managers, on_delete=models.CASCADE)  # pm=Product_managers.name
-    students = models.JSONField()  # В это поле кладем JSON с учениками
-    time = models.ForeignKey(Students, on_delete=models.CASCADE)
-    level = IntegerField()  
+    team_name = models.TextField()
+    pm_name = models.ForeignKey(ProductManagers, on_delete=models.CASCADE)
+    students = models.JSONField()
+    time_slot = models.DateTimeField()
+    team_level = models.CharField(max_length=50)
+    team_chat_username = models.CharField(max_length=50)
+    project = models.ForeignKey(Projects, on_delete=models.CASCADE)
+
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+"""
+Этот модуль обрабатывает сценариии работы бота с сущностями БД
+"""
+
+"""
+------------------------------------------------------------------------------
+Ниже можем реализовывать разные сценарии работы бота с БД
+------------------------------------------------------------------------------
+"""
 from datetime import datetime
-import sys
 import os
+import sys
 
 import django
+
 
 
 sys.dont_write_bytecode = True
@@ -10,24 +20,47 @@ sys.dont_write_bytecode = True
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
-from db.models import Product_managers, Projects, Students 
+from db.models import *
 
-"""Закакзчик анонсирует проект"""
+
+"""Заказчик анонсирует проект"""
 Projects.objects.create(
-    name='Автоматизация формирования групповых проектов',
-    date_start=datetime.fromisoformat('2022-05-13'),
-    date_end=datetime.fromisoformat('2022-05-20'),
+    project_name='Автоматизация формирования групповых проектов',
+    project_start_date=datetime.fromisoformat('2022-05-13'),
+    project_end_date=datetime.fromisoformat('2022-05-20')
 )
+
 
 """Бот проводит опрос продукт-менеджеров"""
-Product_managers.objects.create(
-    name='Игорь Суровый',
-    tg_id=54322381,
-    tg_nick='igsu',
-    time_slot=datetime.fromisoformat('2022-05-20 20:00'),
+ProductManagers.objects.create(
+    pm_name='Игорь Суровый',
+    pm_tg_id=54322381,
+    pm_tg_username='@igsu',
+    start_work_time=datetime.fromisoformat('2022-05-13 20:00'),
+    end_work_time=datetime.fromisoformat('2022-05-13 22:00')
 )
 
-"""Бот проводит опрос студентов"""
+
+ProductManagers.objects.create(
+    pm_name='Василий Смирнов',
+    pm_tg_id=5432238123423,
+    pm_tg_username='@vsmir',
+    start_work_time=datetime.fromisoformat('2022-05-13 18:00'),
+    end_work_time=datetime.fromisoformat('2022-05-13 21:00')
+)
+
+
+ProductManagers.objects.create(
+    pm_name='Петр Непервый',
+    pm_tg_id=54322342345111,
+    pm_tg_username='@1petr',
+    start_work_time=datetime.fromisoformat('2022-05-13 17:00'),
+    end_work_time=datetime.fromisoformat('2022-05-13 20:00')
+)
+
+
+"""
+Бот проводит опрос студентов
 Students.objects.create(
     name='Вася Петров',
     tg_id=35789213,
@@ -47,7 +80,5 @@ Students.objects.create(
     tg_id=35789213,
     level=1,
     time_slot=datetime.fromisoformat('2022-05-20 20:00'),
-
-
 )
-
+"""
